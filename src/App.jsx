5becc6a6016b820790d1b872807f27cfc1894db4
Ramakrishna Katragadda <ramakrishna.katragadda@bottomline.com>
2023-03-22 18:50:04 +0530

    feat:Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import { createContext, useState } from 'react';
 // import Input from 'atoms/Input';
 // import Avatar from 'atoms/Avatar';
+// import Navbar from './components/Navbar';
 
 const ThemeContext = createContext(null);
 
 const App = () => {
-  const [theme, setTheme] = useState('dark')
+  const [theme, setTheme] = useState('light')
 
   return (
     <ThemeContext.Provider value={theme} setTheme={setTheme}>
       <div className={theme === 'dark' ? 'theme__dark' : 'theme__light'}>
         {/* <Input /> */}
+        {/* <Navbar /> */}
         {/* <Avatar 
           imgSrc="https://source.unsplash.com/user/c_v_r/100x100"
           isActive={true}
