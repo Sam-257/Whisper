5cd9ddb507e720b09f66f06353fbc04542904e82
Sam-257 <sambhavsurana257@gmail.com>
2023-03-22 18:23:29 +0530

    Initial work for Badge

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createContext, useState } from 'react';
 // import Input from 'atoms/Input';
 // import Avatar from 'atoms/Avatar';
+// import Badge from 'atoms/Badge';
 
 const ThemeContext = createContext(null);
 
@@ -16,6 +17,7 @@ const App = () => {
           isActive={true}
           clickable={false}
         /> */}
+        {/* <Badge /> */}
       </div>
     </ThemeContext.Provider>
   );
